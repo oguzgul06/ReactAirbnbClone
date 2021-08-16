3503c1e21e64eead949e9fb50cef95ac9d4bf589
oguz gül <ogzgl95@outlook.com>
2021-08-16 10:24:49 +0300

    Create Header and Home component and Build Airbnb Icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React from "react";
 import "./App.css";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App">
-    <h1>Let's build Airbnb Clone 🚀</h1>
-  </div>;
+  return (
+    <div className="app">
+      {/* Home */}
+      <Header />
+      <Home />
+      {/* Header */}
+
+      {/* Banner */}
+      {/* Search */}
+
+      {/* Cards */}
+
+      {/* Footer */}
+
+      {/* SearchPage */}
+      {/* Header */}
+    </div>
+  );
 }
 
 export default App;
