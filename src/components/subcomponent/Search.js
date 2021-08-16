9db84696348d3dd9712ceef4c401bb2498a8aef9
oguz gül <ogzgl95@outlook.com>
2021-08-16 13:37:10 +0300

    Build Date Picker component on the Search.js

--- src/components/subcomponent/Search.js
+++ src/components/subcomponent/Search.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import "./Search.css"
+import React, { useState } from "react";
+import "./Search.css";
+import { DateRangePicker } from "react-date-range";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/thema/default.css";
 
+// DATE PICKER COMPONENT
 function Search() {
-    return (
-        <div className="search">
-            
-        </div>
-    )
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const SelectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  function handleSelect(ranges) {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  }
+
+  return (
+    <div className="search">
+      <DateRangePicker ranges={[SelectionRange]} onChange={handleSelect} />
+    </div>
+  );
 }
 
-export default Search
+export default Search;
