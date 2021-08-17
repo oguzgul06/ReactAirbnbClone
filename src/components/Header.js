84afdf6a1f9cbb5b9f36b6bd68767c0ff33cc0b5
oguz gül <ogzgl95@outlook.com>
2021-08-17 13:53:22 +0300

    Get to Results page without refreshing

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,18 @@ import SearchIcon from "@material-ui/icons/Search";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import LanguageIcon from '@material-ui/icons/Language';
 import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
+      <Link to="/">
       <img
         className="header__icon"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png"
         alt=""
       />
+      </Link>
 
       <div className="header__center">
         <input type="text" />
