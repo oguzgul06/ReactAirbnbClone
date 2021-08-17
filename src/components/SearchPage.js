252ce251a15b65dbc4cd7ed759be0782b4aa19c9
oguz gül <ogzgl95@outlook.com>
2021-08-17 12:47:31 +0300

    Build SearchPage component and create SearchResult component

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import "./SearchPage.css";
+import { Button } from "@material-ui/core";
+import SearchResult from "./subcomponent/SearchResult";
 
 function SearchPage() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="searchPage">
+      <div className="searchPage__info">
+        <p>62 stays - 26 augustto 30 august - 2 guest</p>
+        <h1>Stays nearby</h1>
+
+        <Button variant="outlined">Cancellation Flexibility</Button>
+
+        <Button variant="outlined">Type of place</Button>
+
+        <Button variant="outlined">Price</Button>
+
+        <Button variant="outlined">Rooms and beds</Button>
+
+        <Button variant="outlined">More filters</Button>
+      </div>
+
+      <SearchResult />
+    </div>
+  );
 }
 
-export default SearchPage
+export default SearchPage;
