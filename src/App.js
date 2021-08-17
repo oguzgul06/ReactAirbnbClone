f0137924d4b6a906eaca6d96710c46d4a62e3b4c
oguz gül <ogzgl95@outlook.com>
2021-08-17 12:01:23 +0300

    Create SearchPage and Build React-Router-DOM

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 import Home from "./components/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./components/SearchPage";
 
 function App() {
   return (
     <div className="app">
-      {/* Home */}
-      <Header />
-      <Home />
-      <Footer />
-      {/* Header */}
+      <Router>
+        <Header />
 
-      {/* Banner */}
-      {/* Search */}
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
 
-      {/* Cards */}
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
 
-      {/* Footer */}
-
-      {/* SearchPage */}
-      {/* Header */}
+        <Footer />
+      </Router>
     </div>
   );
 }
